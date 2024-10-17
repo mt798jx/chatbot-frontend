1c45f8c3d98760a03735f55c2da283a5b3940b56
Tvrdomir <mtvrdon@outlook.sk>
2024-10-17 12:47:07 +0200

    back to graphs 1.12

--- src/components/file/MultipleScoreComparisonCharts.js
+++ src/components/file/MultipleScoreComparisonCharts.js
@@ -36,7 +36,19 @@ const MultipleScoreComparisonCharts = ({ language }) => {
     }, []);
 
     return (
-        <Box sx={{ padding: 4 }}>
+        <Box sx={{
+            margin: 2,
+            padding: 2,
+            border: '1px solid',
+            borderColor: 'divider',
+            borderRadius: 1,
+            backgroundColor: 'background.paper',
+            boxShadow: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            maxHeight: 260,
+            overflow: 'hidden'
+        }}>
             {loading ? (
                 <Typography variant={isSmallScreen ? "body2" : "body1"} sx={{ color: 'warning.main' }}>
                     {language === 'en' ? "Loading charts..." : "Načítavajú sa grafy..."}
@@ -56,13 +68,14 @@ const MultipleScoreComparisonCharts = ({ language }) => {
                             borderRadius: 1,
                             backgroundColor: 'background.paper',
                             boxShadow: 1,
+                            width: '100%',
                         }}
                     >
                         <Typography variant={isSmallScreen ? "h6" : "h5"} gutterBottom>
                             {language === 'en' ? `Comparison for ${fileName}` : `Porovnanie pre ${fileName}`}
                         </Typography>
                         <Box sx={{ width: '100%', height: 400, marginTop: 2 }}>
-                            <ResponsiveContainer width="100%" height={400}>
+                            <ResponsiveContainer width="100%" height={400}>  {/* Full width */}
                                 <BarChart data={data}>
                                     <CartesianGrid strokeDasharray="3 3" />
                                     <XAxis dataKey="range" />
